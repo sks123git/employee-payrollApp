d3c998ffdd50a28e33f3c76c9c4b3a2139f8fdfa
subzero <shubham8111994@gmail.com>
2023-04-20 11:04:44 +0530

    linking to json server

--- script.js
+++ script.js
@@ -213,6 +213,53 @@ function save() {
     'employeePayrollData',
     JSON.stringify(employeePayrollList)
   );
+  let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+  function makePromiseCall(methodType, url, async = true, data = null) {
+    return new Promise((resolve, reject) => {
+      let xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function () {
+        console.log(
+          'State changed called ready state: ' +
+            xhr.readyState +
+            ' Status:' +
+            xhr.status
+        );
+        if (xhr.status.toString().match('^[2][0-9]{2}$')) {
+          resolve(xhr.responseText);
+        } else if (xhr.status.toString().match('^[4,5][0-9]{2}$')) {
+          reject({ status: xhr.status, statusText: xhr.statusText });
+          console.log('xhr failed');
+        }
+      };
+
+      xhr.open(methodType, url, async);
+      if (data) {
+        console.log(JSON.stringify(data));
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(data));
+      } else xhr.send();
+      console.log(methodType + ' request sent to server');
+    });
+  }
+
+  const postURL = 'http://localhost:3000/employeesPayrollDB';
+  const newDetail = {
+    id: newEmployee.id,
+    name: newEmployee.name,
+    salary: newEmployee.salary,
+    profileImg: newEmployee.profileImg,
+    gender: newEmployee.gender,
+    department: newEmployee.department,
+    startDate: newEmployee.startDate,
+    notes: newEmployee.notes,
+  };
+  makePromiseCall('POST', postURL, true, newDetail)
+    .then((responseText) => {
+      console.log('data is inserted: ' + responseText);
+    })
+    .catch((error) => {
+      console.log('Post error caught: ' + error);
+    });
 }
 
 function checkForUpdate() {
